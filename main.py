09e18e53b2d5d18c8452eb07c821dd2520d353c9
Phakphum Artkaew <pma5244@psu.edu>
2024-04-20 02:39:18 -0400

    babyberta config

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
-#from transformers import RobertaConfig, RobertaForMaskedLM, Trainer, TrainingArguments
-#from tokenizers import ByteLevelBPETokenizer
-#from torch.utils.data import Dataset
-#from transformers import PreTrainedTokenizerFast
+import random
+import torch
+from pathlib import Path
+from transformers import RobertaForMaskedLM, RobertaConfig, Trainer, TrainingArguments, DataCollatorForLanguageModeling
+from babyberta import load_tokenizer, Params
+from babyberta.dataset import DataSet
 from tokenizers import Tokenizer
+from params import params
+
+class Data:
+    min_sentence_length = 3
+    train_prob = 1.0  # probability that sentence is assigned to train split
+    mask_symbol = '<mask>'
+    pad_symbol = '<pad>'
+    unk_symbol = '<unk>'
+    bos_symbol = '<s>'
+    eos_symbol = '</s>'
+    roberta_symbols = [mask_symbol, pad_symbol, unk_symbol, bos_symbol, eos_symbol]
 
 class BabyBERTaMaxTrainer:
     def __init__(self):
-        pass
+        self.tokenizer = self.loadTokenizer
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        #Configuration parameters from BabyBERTa
+        config = RobertaConfig(vocab_size=len(self.tokenizer.get_vocab()),
+                               pad_token_id=self.tokenizer.token_to_id(Data.pad_symbol),
+                               bos_token_id=self.tokenizer.token_to_id(Data.bos_symbol),
+                               eos_token_id=self.tokenizer.token_to_id(Data.eos_symbol),
+                               return_dict=True,
+                               is_decoder=False,
+                               add_cross_attention=False,
+                               layer_norm_eps=params.layer_norm_eps,
+                               max_position_embeddings=params.max_input_length + 2,
+                               hidden_size=params.hidden_size,
+                               num_hidden_layers=params.num_layers,
+                               num_attention_heads=params.num_attention_heads,
+                               intermediate_size=params.intermediate_size,
+                               intializer_range=params.initializer_range
+                               )
+        self.model = RobertaForMaskedLM(config=config)
+        self.model.to(device)
+    def loadTokenizer(self):
+        tokenizer = Tokenizer.from_file(str("tokenizer/babyberta.json"))
+        tokenizer.enable_truncation(max_length=params.max_input_length)
+        return tokenizer
     def initializeModel(self):
         pass
     def trainModel(self):
@@ -15,9 +51,4 @@ class BabyBERTaMaxTrainer:
         pass
 
 if __name__ == "__main__":
-    #Configuration parameters from BabyBERTa
-    #config = RobertaConfig()
-    #Load tokenizer from BabyBERTa
-    tokenizer = Tokenizer.from_file(str("tokenizer/babyberta.json"))
-    tokenizer.enable_truncation(max_length=128)
-    vocabSize = len(tokenizer.get_vocab())
+
---
+++ params.py
@@ -0,0 +1,36 @@
+#params from BabyBERTa
+class params:
+    # Data related settings
+    sample_with_replacement = False
+    training_order = 'original'
+    consecutive_masking = False
+    num_sentences_per_input = 1
+    include_punctuation = True
+    allow_truncated_sentences = False
+    num_mask_patterns = 10
+    mask_pattern_size = 2
+    probabilistic_masking = True
+    mask_probability = 0.15
+    leave_unmasked_prob_start = 0.0
+    leave_unmasked_prob = 0.0
+    random_token_prob = 0.1
+    corpora = ('aochildes',)
+    tokenizer = 'babyberta'
+    add_prefix_space = True
+    max_input_length = 128
+
+    # Training specific settings
+    batch_size = 16
+    lr = 1e-4
+    num_epochs = 1
+    num_warmup_steps = 24000
+    weight_decay = 0.0
+
+    # Model configuration
+    load_from_checkpoint = 'none'
+    hidden_size = 256
+    num_layers = 8
+    num_attention_heads = 8
+    intermediate_size = 1024
+    initializer_range = 0.02
+    layer_norm_eps = 1e-5
