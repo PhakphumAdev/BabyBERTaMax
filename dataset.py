4847c41324a7ac3e355ef8374f498f8f686ad55d
Phakphum Artkaew <pma5244@psu.edu>
2024-05-04 02:26:01 -0400

    sentences

--- dataset.py
+++ dataset.py
@@ -5,6 +5,8 @@ class babyDataset:
     def __init__(self, filepath, tokenizer):
         self.filepath = filepath
         self.tokenizer = tokenizer
-        self.dataset = load_dataset('text', data_files=filepath)
+        raw_dataset = load_dataset('text', data_files=filepath)
+        sentences = raw_dataset['train']['text']
+        self.dataset = ' '.join(sentences)
         # use babyberta tokenizer to tokenize the dataset
         self.tokenized_dataset = tokenizer.encode(self.dataset, add_special_tokens=False).tokens
